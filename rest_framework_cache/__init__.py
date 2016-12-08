14e515483e93739d9e440b4800f8041c061cf0ce
Juan D <juan.drucker@toptal.com>
2016-12-08 16:34:20 -0300

    Update __init__.py

--- rest_framework_cache/__init__.py
+++ rest_framework_cache/__init__.py
@@ -1,5 +1,7 @@
+import pkg_resources
 __author__ = 'Ervilis Souza'
 __email__ = 'ervilisviana@gmail.com'
 __version__ = '0.1.1'
 
+pkg_resources.declare_namespace(__name__)
 default_app_config = 'rest_framework_cache.apps.RestFrameworkCacheConfig'
